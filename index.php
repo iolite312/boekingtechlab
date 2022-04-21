76dd0124ca6e2ce9905f23b116f8a9aef90c69ee
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-04-21 15:39:22 +0200

    update to default

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <?php echo 'Fuck! This shit is bussin' ?>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel eos minus atque cupiditate ipsum quam fugit sunt at blanditiis est ut maxime incidunt ea ullam in, deserunt quod dicta dolor.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eos minus atque cupiditate ipsum quam fugit sunt at blanditiis est ut maxime incidunt ea ullam in, deserunt quod dicta dolor.</p>
 </body>
 </html>0
