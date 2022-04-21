58a4c39b995118a3063ef5254d16d26669027d0c
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-04-21 15:22:00 +0200

    hey Leon

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo 'Fuck! This shit is bussin' ?>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel eos minus atque cupiditate ipsum quam fugit sunt at blanditiis est ut maxime incidunt ea ullam in, deserunt quod dicta dolor.</p>
+</body>
+</html>
