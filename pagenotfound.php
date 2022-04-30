06d75ad7754f252e9874608a3895ba47dd34e4af
superman312 <68647023+superman312@users.noreply.github.com>
2022-04-30 14:05:00 +0200

    we found shit

--- pagenotfound.php
+++ pagenotfound.php
@@ -1,1 +1,1 @@
-<p>not found shit</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/g3iFJpGJiug?start=26" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
