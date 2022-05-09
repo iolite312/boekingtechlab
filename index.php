e66a1db5d6d44c16fad548ac84dad82dc871f178
Natfaji_A <modemoon2003@hotmail.com>
2022-05-09 11:25:37 +0200

    micro change

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <title>Techlab Regius College</title>
-
-    <!-- Stylesheets -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="/css/alerts.css">
 </head>
