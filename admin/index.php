31d9746e96bf0edafd0042ff388c63abeee22a1d
Natfaji_A <modemoon2003@hotmail.com>
2022-05-10 14:11:37 +0200

    reseviring admin test

--- admin/index.php
+++ admin/index.php
@@ -12,8 +12,7 @@ if (!isset($_SESSION)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>booking</title>
-    <!-- <link rel="icon" type="image/png" href="/assets/logo/LOGO.png" />
-	<link rel="stylesheet" href="/css/booking.css">-->
+    <!-- <link rel="icon" type="image/png" href="/assets/logo/LOGO.png" /> -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/alerts.css">
@@ -30,7 +29,30 @@ if (!isset($_SESSION)) {
             <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/php/inc/messages.inc.php' ?>
         </section>
 
-        hello there
+        <section>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Naam</th>
+                    <th>Email</th>
+                    <th>Organisatie</th>
+                    <th>Lokaal</th>
+                    <th>Materiaal</th>
+                    <th>Tijd</th>
+                    <th>Verwijderen</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>menner hello</td>
+                    <td>email@email.com</td>
+                    <td>ROC</td>
+                    <td>Lokaal 1</td>
+                    <td>PC en VR</td>
+                    <td>12:30-01:30</td>
+                    <td><button type="submit">Delete</button></td>
+                </tr>
+            </table>
+        </section>
     </main>
 </body>
 
