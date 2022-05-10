71ac4b3e8a7aa343d643b20a9d69e379e5be8f65
Natfaji_A <modemoon2003@hotmail.com>
2022-05-10 13:42:20 +0200

    micro changes to header

--- php/inc/header.inc.php
+++ php/inc/header.inc.php
@@ -1,6 +1,6 @@
     <h1>Techlab Regius College</h1>
     <div class="btn">
-    	<a class="<?php if ($page == 'home') {echo 'active';}; ?>" href="index.php">Home</a>
-    	<a class="<?php if ($page == 'reserveren') {echo 'active';}; ?>" href="#">Reserveren</a>
-    	<a href="/loginpage" class="<?php if ($page == 'login') {echo 'active';}; ?>">Login</a>
+    	<a class="<?php if ($page == 'home') {echo 'active';} ?>" href="/index">Home</a>
+    	<a class="<?php if ($page == 'reserveren') {echo 'active';} ?>" href="/booking">Reserveren</a>
+    	<a class="<?php if ($page == 'login') {echo 'active';} ?>" href="/loginpage">Login</a>
 	</div>
