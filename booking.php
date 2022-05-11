5dffb2db3b10ad0ff2f1f77fa68e14bfbc2c6ef4
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-05-11 14:31:08 +0200

    Didn't work

--- booking.php
+++ booking.php
@@ -39,24 +39,20 @@ $page = 'reserveren'
 				<form action="/php/booking.php" method="post" required>
 					<div class="organisatie">
 						<h3>Organisatie</h3>
-						<div class="left">
-							<input id="ROC" type="radio" name="organisatie" value="ROC Kop van Noord-Holland" checked>
-							<label for="ROC">ROC Kop van Noord-Holland</label><br>
-							<input id="regiuscollege" type="radio" name="organisatie" value="Regius College">
-							<label for="regiuscollege">Regius College</label><br>
-						</div>
-						<div class="right">
-							<select name="ROCKopNH" id="rockopnh">
-								<option value="hofstraat">Hofstraat</option>
-								<option value="denhelder">Den Helder</option>
-							</select>
-							<select name="RegiusCollege" id="regius">
-								<option value="">Wilheminalaan</option>
-								<option value="">Oranjelaan</option>
-								<option value="">Emmalaan</option>
-								<option value="">Hofstraat</option>
-							</select>
-						</div>
+						<input id="ROC" type="radio" name="organisatie" value="ROC Kop van Noord-Holland" checked>
+						<label for="ROC">ROC Kop van Noord-Holland</label><br>
+						<input id="regiuscollege" type="radio" name="organisatie" value="Regius College">
+						<label for="regiuscollege">Regius College</label><br>
+						<select name="ROCKopNH" id="rockopnh">
+							<option value="hofstraat">Hofstraat</option>
+							<option value="denhelder">Den Helder</option>
+						</select>
+						<select name="RegiusCollege" id="regius">
+							<option value="">Wilheminalaan</option>
+							<option value="">Oranjelaan</option>
+							<option value="">Emmalaan</option>
+							<option value="">Hofstraat</option>
+						</select>
 					</div>
 					<input type="email" name="organization_email" placeholder="Email"><br>
 
