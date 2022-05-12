dab554de382af1827887bf4b5cbb252a156cd692
Eric <258753@cursist.rockopnh.nl>
2022-05-12 11:54:12 +0200

    Leon read the message below please

--- php/inc/footer.inc.php
+++ php/inc/footer.inc.php
@@ -1,11 +1,11 @@
 <div class="footercontent">
-    <p style="font-size: large;">Contact</p>
+    <p style="font-size: larger;">Contact</p>
     <p>placeholder@placeholder.com <br>
     06-123456789 <br>
     Adres Placeholder
     </p>
 </div>
 <div class="footercontent">
-    <p style="font-size: large;">Over het Techlab</p>
+    <p style="font-size: larger;">Over het Techlab</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae autem animi saepe id cumque illum delectus debitis nemo eos.</p>
 </div>
