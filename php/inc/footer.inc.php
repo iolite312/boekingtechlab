71886a2f4ef52e93c230e00aa8cc69d38c5b066d
Eric <258753@cursist.rockopnh.nl>
2022-05-12 11:52:55 +0200

    leon fix your sass

--- php/inc/footer.inc.php
+++ php/inc/footer.inc.php
@@ -1,10 +1,11 @@
 <div class="footercontent">
-    <p>Contact</p>
+    <p style="font-size: large;">Contact</p>
     <p>placeholder@placeholder.com <br>
     06-123456789 <br>
     Adres Placeholder
     </p>
 </div>
 <div class="footercontent">
-    <p>Over het Techlab <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae autem animi saepe id cumque illum delectus debitis nemo eos.</p>
+    <p style="font-size: large;">Over het Techlab</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae autem animi saepe id cumque illum delectus debitis nemo eos.</p>
 </div>
