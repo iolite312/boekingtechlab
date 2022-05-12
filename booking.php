44281aa091e18f1247b0160462a967e9a31b73a1
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-05-12 11:39:32 +0200

    specificity increase!

--- booking.php
+++ booking.php
@@ -44,8 +44,8 @@ $page = 'reserveren'
 						<label for="ROC">ROC Kop van Noord-Holland</label><br>
 						<input id="regiuscollege" type="radio" name="organisatie" value="Regius College">
 						<label for="regiuscollege">Regius College</label><br>
-						<input id="anders" type="radio" name="organisatie" value="anders">
-						<label for="anders">Anders</label><br>
+						<input id="anders-vraag" type="radio" name="organisatie" value="anders-vraag">
+						<label for="anders-vraag">Anders</label><br>
 						<select name="ROCKopNH" id="rockopnh">
 							<option value="hofstraat">Schagen</option>
 							<option value="denhelder">Den Helder</option>
@@ -56,7 +56,7 @@ $page = 'reserveren'
 							<option value="">Emmalaan</option>
 							<option value="">Hofstraat</option>
 						</select>
-						<input type="text" name="anders" id="anders-antwoord">
+						<input type="text" name="anders-antwoord" id="anders-antwoord">
 					</div>
 					<input type="email" name="organization_email" placeholder="Email"><br>
 
