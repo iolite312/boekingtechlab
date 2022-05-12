7d84994ab2a626b826bd188de216cedbe9fef47e
superman312 <68647023+superman312@users.noreply.github.com>
2022-05-12 14:29:32 +0200

    yes

--- user.php
+++ user.php
@@ -1,11 +1,15 @@
 <?php
-if ($_SESSION['user_level'] == 1) {
-    header('Location: /admin');
+if ($_SESSION['user_level'] === 1) {
+    echo 1;
 } else {
     echo 2;
 }
+echo '<br>';
 if ($_SESSION['user_level'] === 0) {
     echo 3;
 } else {
     echo 4;
 }
+echo '<br>';
+
+echo $_SESSION['user_level'];
