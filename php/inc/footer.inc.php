ae730d43329e38b67e5803f1401eee6bee3bab3e
Eric <258753@cursist.rockopnh.nl>
2022-05-12 11:27:50 +0200

    i did the max width wrong because im an idiot

--- php/inc/footer.inc.php
+++ php/inc/footer.inc.php
@@ -1,10 +1,10 @@
-<div>
+<div class="footercontent">
     <p>Contact</p>
     <p>placeholder@placeholder.com <br>
     06-123456789 <br>
     Adres Placeholder
     </p>
 </div>
-<div>
+<div class="footercontent">
     <p>Over het Techlab <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae autem animi saepe id cumque illum delectus debitis nemo eos.</p>
 </div>
