83679412eb1cc5ca589c6fe268db81ce39bde1c7
Natfaji_A <modemoon2003@hotmail.com>
2022-05-12 09:37:06 +0200

    fix removebooking

--- php/removebooking.php
+++ php/removebooking.php
@@ -1,8 +1,7 @@
 <?php
-echo $_SESSION['user_level'];
-if ($_SESSION['user_level'] === 1)
-echo 2;
-exit;
+if (!isset($_SESSION)) {
+    session_start();
+}
 if (isset($_POST["submit"]) && $_SESSION['user_level'] === 1) {
 
     //start database connection
@@ -11,9 +10,6 @@ if (isset($_POST["submit"]) && $_SESSION['user_level'] === 1) {
     //connect to functions.php
     require_once $_SERVER["DOCUMENT_ROOT"] . '/php/function.php';
 
-    if (!isset($_SESSION)) {
-        session_start();
-    }
 
     $bookingID = $_POST['bookingID'];
     removebooking($bookingID);
