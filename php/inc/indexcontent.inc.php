be20cb9e3c4858ebedffead6d9d26a0ff8e76ee3
Eric <258753@cursist.rockopnh.nl>
2022-05-13 11:47:26 +0200

    i forgot to actually add the buttons

--- php/inc/indexcontent.inc.php
+++ php/inc/indexcontent.inc.php
@@ -41,6 +41,11 @@
   <div class="text">VR ruimte</div>
 </div>
 
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
 
 </div>
 <br>
