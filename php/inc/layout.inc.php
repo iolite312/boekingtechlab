4526599b4a8bb3784fc9a347081a4a679e9e9cde
superman312 <68647023+superman312@users.noreply.github.com>
2022-05-13 10:24:08 +0200

    map?

---
+++ php/inc/layout.inc.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="map.css">
+<img src="/assets/images/layout.png" usemap="#image_map">
+<map name="image_map">
+  <area alt="Lab 1" title="Lab 1" href="#" coords="611,47 611,47 611,427 836,428 836,374 878,322 940,304 943,49 943,49 943,49 " shape="polygon">
+  <area alt="test" title="test" href="#" coords="1275,65 1281,50 1281,480 1073,482 1082,433 1063,360 1016,304 947,286 939,49 1283,49 " shape="polygon">
+  <area alt="Lab 2" title="Lab 2" href="#" coords="608,436 608,436 609,814 937,814 943,559 878,538 838,497 833,437 " shape="polygon">
+  <area alt="Greenroom" title="Greenroom" href="#" coords="1277,506 1154,506 1113,550 1111,689 1281,686 " shape="polygon">
+  <area alt="VR room" title="VR room" href="#" coords="590,537 92,538 90,648 99,701 122,750 162,792 205,815 258,820 593,820 " shape="polygon">
+  <area alt="Test" title="Test" href="#" coords="944,310 882,325 841,368 841,494 882,535 942,550 1002,540 1047,495 1059,431 1044,372 1004,328 946,310 " shape="polygon">
+</map>
