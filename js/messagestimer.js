f1905992cbc0d0ae8bb346b53ae020db02203839
Natfaji_A <modemoon2003@hotmail.com>
2022-05-13 10:23:19 +0200

    changes on messages timer

--- js/messagestimer.js
+++ js/messagestimer.js
@@ -1,13 +1,8 @@
-showSlides();
+setTimeout(showSlides, 10000); // Change image every 2 seconds
 
 function showSlides() {
-    setTimeout(showSlides, 10000); // Change image every 2 seconds
     let messageContainer = document.getElementById('message-container')
     if (messageContainer) {
         messageContainer.classList.add('fade')
-        console.log(1)
-    } else {
-        console.log(2)
     }
-
 }
