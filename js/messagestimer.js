9b1b0f9091d59b903a9ba9fa311aa2622a31caca
Natfaji_A <modemoon2003@hotmail.com>
2022-05-13 10:19:03 +0200

    fix

--- js/messagestimer.js
+++ js/messagestimer.js
@@ -5,9 +5,9 @@ function showSlides() {
     let messageContainer = document.getElementById('message-container')
     if (messageContainer) {
         messageContainer.classList.add('fade')
-        print(1)
+        console.log(1)
     } else {
-        print(1)
+        console.log(2)
     }
 
 }
