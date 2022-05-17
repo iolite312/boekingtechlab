3488673bff6b156c0aaab31fdbfaa5374ba70f19
Sebastian Hoogendijk <sebastianhoogendijk@gmail.com>
2022-05-17 12:35:55 +0200

    popup 😊🤷‍♂️🎶🎂🎂🤳💕🎶😃🐱‍💻😳🥴🤯🙁☹

---
+++ php/inc/popup.inc.php
@@ -0,0 +1,14 @@
+<div id="pop-up-container">
+    <div id="pop-up">
+        <div id="cross" onclick="ClosePopUp();">
+            <img src="/assets/cross.svg" alt="My Happy SVG" />
+        </div>
+        <p>Het lijkt erop dat je al ingelogt bent, Wil je jouw gegevens invullen?</p>
+        <div id="pop-up-buttons">
+            <form id="pop-up-button" action="pop-up-button-yes" method="get">
+                <input type="button" id="pop-up-button-yes" class="pop-up-button" value="Yes">
+            </form>
+            <button class="pop-up-button" onclick="ClosePopUp();">No</button>
+        </div>
+    </div>
+</div>
