09ef183d29f39bb3cc1d836e5489a8a98867e183
superman312 <68647023+superman312@users.noreply.github.com>
2022-05-17 14:17:17 +0200

    test

---
+++ php/inc/importaccount.inc.php
@@ -0,0 +1,26 @@
+<?php
+//start database connection
+require_once $_SERVER["DOCUMENT_ROOT"] . '/database/db_connection.php';
+
+$sql = "SELECT * FROM users";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    //output data from every row selected and inserts it into the container
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "
+            <form action='/php/removebooking.php' method='post'>
+                <tr>
+                    <input type='hidden' name='bookingID' value=" . $row['id'] . ">
+                    <td>" . $row['id'] . "</td>
+                    <td>" . $row['firstname'] + $row['lastname'] . "</td>
+                    <td>" . $row['email'] . "</td>
+                    <td><input class='button' type='submit' name='submit' value='Verwijderen'></td> 
+                </tr>
+            </form>
+        ";
+    }
+}
+
+mysqli_close($conn);
