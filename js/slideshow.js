a4838e654dfb99ce4ec04fdea67d372bb5de2b83
Pascal <guardian96play@gmail.com>
2022-05-17 11:05:31 +0200

    test 4

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,24 +1,29 @@
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    let i;
+function showSlides(i = 0) {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    if (i < slides.length) {
+        slides[i].style.display = "none";
+        i++;
+    } else {
+        i = 0;
     }
 
     slideIndex++;
 
     if (slideIndex > slides.length) { slideIndex = 1 };
 
-    for (i = 0; i < dots.length; i++) {
+    if (i < dots.length) {
         dots[i].className = dots[i].className.replace(" active", "");
-    } 
+        i++
+    } else {
+        i = 0
+    }
     
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setInterval(showSlides, 5000); // Change image every 5 seconds
+    setInterval(showSlides, 1000, i); // Change image every 5 seconds
 }
