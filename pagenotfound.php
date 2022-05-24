a07e91329b620531adf79ec515ec2744765b5d7c
flurbudurbur <sebastianhoogendijk@gmail.com>
2022-05-24 13:15:28 +0200

    Funky Time!

--- pagenotfound.php
+++ pagenotfound.php
@@ -1,1 +1,1 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/g3iFJpGJiug?autoplay=1&start=28" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Sare42uGX1s" title="Funky Time!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
