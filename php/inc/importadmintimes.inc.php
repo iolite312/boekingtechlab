5ea8896e625f7f6c0db2551c1561a17febeca981
superman312 <68647023+superman312@users.noreply.github.com>
2022-06-02 15:26:48 +0200

    no while loop crash

--- php/inc/importadmintimes.inc.php
+++ php/inc/importadmintimes.inc.php
@@ -4,16 +4,12 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/php/function.php';
 $result = fetchadmintimes($result);
 
 $days = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
-
+$i = 0;
 if (mysqli_num_rows($result) > 0) {
 	//output data from every row selected and inserts it into the container
 	while ($row = mysqli_fetch_assoc($result)) {
-        $i = 0;
-        if ($i <= 6) {
-            $days[$i] = $day;
-        }
 		echo "
-            <h3>" . $day . " </h3>
+            <h3>" . $days[$i] . " </h3>
             <form action='' method='post'>
                 <tr>
                     <input type='hidden' name='bookingID' value=" . $row['id'] . ">
