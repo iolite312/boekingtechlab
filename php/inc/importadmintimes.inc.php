63ea2ed9ccfee84285a19beecd0c7fb6f8c05b09
superman312 <68647023+superman312@users.noreply.github.com>
2022-06-02 14:26:38 +0200

    Does it work

--- php/inc/importadmintimes.inc.php
+++ php/inc/importadmintimes.inc.php
@@ -3,10 +3,17 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/php/function.php';
 
 $result = fetchadmintimes($result);
 
+$days = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+
 if (mysqli_num_rows($result) > 0) {
 	//output data from every row selected and inserts it into the container
 	while ($row = mysqli_fetch_assoc($result)) {
+        $i = 0;
+        while ($i <= 6) {
+            $days[$i] = $day;
+        }
 		echo "
+            <h3>" . $day . " </h3>
             <form action='' method='post'>
                 <tr>
                     <input type='hidden' name='bookingID' value=" . $row['id'] . ">
@@ -19,6 +26,7 @@ if (mysqli_num_rows($result) > 0) {
                 </tr>
             </form>
         ";
+        $i++;
 	}
 }
 
