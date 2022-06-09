bafa11ce38a30287e36949fffb7a4b9a04aa713b
Pascal <guardian96play@gmail.com>
2022-06-09 10:23:31 +0200

    made it so the credentials are gotten from somewhere else

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,9 +1,9 @@
 <?php    
     $servername = 'localhost';
-    $username = 'boekingtechlab';
-    $password = 'OieMVT]Om)-(TD5c';
-    $dbname = 'boekingtechlab';
-    $conn = mysqli_connect($servername,$username, $password, $dbname);
+
+    $config = parse_ini_file($_SERVER["DOCUMENT_ROOT"]. '/database/dbConn.ini')
+
+    $conn = mysqli_connect($servername,$config['username'], $config['password'], $config['db']);
     //check that connection happend
     if(mysqli_connect_errno())
     {
